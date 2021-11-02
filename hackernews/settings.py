b4976f2b94966413aba5cb0597f7e5598df6bf85
TheIszer <theiszer@gmail.com>
2021-11-02 07:36:20 +0000

    Queries update

--- hackernews/settings.py
+++ hackernews/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'p*lpvlty@y%u4vfsr8fe0q=eox@s1_x4xd)!@q4mhri)qrj+r4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'graphene_django',
+    'links',
 ]
 
 MIDDLEWARE = [
